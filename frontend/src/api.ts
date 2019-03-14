1468f9092b1263544d7990b98a452233cf939ba8
MrRainwater <n/a>
2019-03-14 18:30:34 -0500

    graphql api w/ bookmarks call

---
+++ frontend/src/api.ts
@@ -0,0 +1,38 @@
+import { IBookmark } from 'interfaces'
+
+async function graphql(query: string, variables?: any) {
+  try {
+    const res = await fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query,
+        variables
+      })
+    })
+    const json = await res.json()
+
+    return json.data
+  } catch (err) {
+    console.error('Error in graphql call')
+    console.error(query)
+    console.error({ variables })
+    throw new Error('Network Error')
+  }
+}
+
+export function bookmarks(): Promise<IBookmark> {
+  return graphql(`
+    {
+      bookmarks {
+        url
+        title
+        description
+        img
+      }
+    }
+  `)
+}
