d2df0d80e3ccad19beb7a08ba4b39be97064388b
Quinzell <jared.rainwater@monsanto.com>
2019-03-31 16:26:47 -0500

    create bookmark dialog

--- frontend/src/components/CreateBookmarkDialog.tsx
+++ frontend/src/components/CreateBookmarkDialog.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import Dialog from 'react-toolbox/lib/dialog'
-import { IBookmarkCreate } from 'interfaces'
+import { IBookmark } from 'interfaces'
 import Input from 'react-toolbox/lib/input'
 
 interface Props {
-  active: boolean
+  metadata: IBookmark
   onCancel: () => void
-  onSave: (bookmark: any) => void
+  onSave: (bookmark: IBookmark) => void
 }
 
-const CreateBookmark: React.FunctionComponent<Props> = ({
-  active,
+const CreateBookmarkDialog: React.FunctionComponent<Props> = ({
+  metadata,
   onCancel,
   onSave
 }) => {
@@ -25,13 +25,9 @@ const CreateBookmark: React.FunctionComponent<Props> = ({
     }
   ]
 
-  const [bookmark, setBookmark] = React.useState<IBookmarkCreate>({
-    title: '',
-    description: '',
-    url: ''
-  })
+  const [bookmark, setBookmark] = React.useState<IBookmark>(metadata)
 
-  function mergeBookmark<T extends IBookmarkCreate, K extends keyof T>(key: K) {
+  function mergeBookmark<T extends IBookmark, K extends keyof T>(key: K) {
     return (val: T[K]) =>
       setBookmark({
         ...bookmark,
@@ -39,34 +35,52 @@ const CreateBookmark: React.FunctionComponent<Props> = ({
       })
   }
 
+  console.log(bookmark)
+
   return (
     <Dialog
+      active={true}
       title="Create a bookmark"
       actions={actions}
-      active={active}
       onEscKeyDown={onCancel}
       onOverlayClick={onCancel}
     >
-      <Input
-        type="text"
-        label="url"
-        value={bookmark.url}
-        onChange={mergeBookmark('url')}
-      />
       <Input
         type="text"
         label="title"
         value={bookmark.title}
         onChange={mergeBookmark('title')}
       />
+
       <Input
         type="text"
         label="description"
         value={bookmark.description}
         onChange={mergeBookmark('description')}
       />
+
+      <Input
+        type="text"
+        label="url"
+        value={bookmark.url}
+        onChange={mergeBookmark('url')}
+      />
+
+      <Input
+        type="text"
+        label="img"
+        value={bookmark.img}
+        onChange={mergeBookmark('img')}
+      />
+
+      <Input
+        type="text"
+        label="logo"
+        value={bookmark.logo}
+        onChange={mergeBookmark('logo')}
+      />
     </Dialog>
   )
 }
 
-export default CreateBookmark
+export default CreateBookmarkDialog
