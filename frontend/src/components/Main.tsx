ac834167662b58c444c7e6c6ee385e040710e5ce
Quinzell <jared.rainwater@monsanto.com>
2019-04-02 16:20:29 -0500

    populate store with data

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,24 +1,17 @@
 import * as React from 'react'
 import { Layout, Panel } from 'react-toolbox/lib/layout'
 import Bookmarks from 'components/Bookmarks'
-import NewBookmarkButton from 'components/NewBookmarkButton'
 import * as classes from './styles/panel.scss'
-import { IBookmarkCreate, IBookmark } from 'interfaces'
 import { getBookmarks } from 'api'
 import AppBar from './AppBar'
+import { useBookmarks } from 'hooks'
 
 const Main: React.FunctionComponent = () => {
-  const [isCreateActive, setIsCreateActive] = React.useState(false)
-  const [bookmarks, setBookmarks] = React.useState<IBookmark[]>([])
+  const [bookmarks, setBookmarks] = useBookmarks()
 
   React.useEffect(() => {
     getBookmarks().then(setBookmarks)
   }, [])
-
-  function saveBookmark(bookmark: IBookmarkCreate) {
-    console.log(bookmark)
-    setIsCreateActive(false)
-  }
 
   return (
     <Layout>
