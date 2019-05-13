b2c09229feb638963d57420f736e4a6d0b01c76c
Quinzell <jared.rainwater@monsanto.com>
2019-05-13 07:20:29 -0500

    use some of the stored data

--- backend/src/bookmarks.ts
+++ backend/src/bookmarks.ts
@@ -1,45 +1,8 @@
-const bookmarks = [
-  {
-    url: 'https://www.discordapp.com',
-    title: 'Discord - Free Voice and Text Chat',
-    description:
-      'Step up your game with a modern voice & text chat app. Crystal clear voice, multiple server and channel support, mobile apps, and more.',
-    img:
-      'https://www.discordapp.com/assets/ba74954dde74ff40a32ff58069e78c36.png'
-  },
-  {
-    url: 'https://www.discordapp.com',
-    title: 'Discord - Free Voice and Text Chat (1)',
-    description:
-      'Step up your game with a modern voice & text chat app. Crystal clear voice, multiple server and channel support, mobile apps, and more.',
-    img:
-      'https://www.discordapp.com/assets/ba74954dde74ff40a32ff58069e78c36.png'
-  },
-  {
-    url: 'https://www.discordapp.com',
-    title: 'Discord - Free Voice and Text Chat (2)',
-    description:
-      'Step up your game with a modern voice & text chat app. Crystal clear voice, multiple server and channel support, mobile apps, and more.',
-    img:
-      'https://www.discordapp.com/assets/ba74954dde74ff40a32ff58069e78c36.png'
-  },
-  {
-    url: 'https://www.discordapp.com',
-    title: 'Discord - Free Voice and Text Chat (3)',
-    description:
-      'Step up your game with a modern voice & text chat app. Crystal clear voice, multiple server and channel support, mobile apps, and more.',
-    img:
-      'https://www.discordapp.com/assets/ba74954dde74ff40a32ff58069e78c36.png'
-  },
-  {
-    url: 'https://www.discordapp.com',
-    title: 'Discord - Free Voice and Text Chat (4)',
-    description:
-      'Step up your game with a modern voice & text chat app. Crystal clear voice, multiple server and channel support, mobile apps, and more.',
-    img:
-      'https://www.discordapp.com/assets/ba74954dde74ff40a32ff58069e78c36.png'
-  }
-]
+import { readFileSync } from 'fs'
+
+const bookmarks = JSON.parse(
+  readFileSync('data/bookmarks.json').toString()
+) as any[]
 
 interface Bookmark {
   url: string
@@ -49,7 +12,7 @@ interface Bookmark {
 }
 
 export default class BookmarkStore {
-  data: Bookmark[] = bookmarks
+  data: Bookmark[] = bookmarks.slice(0, 50)
 
   add(bookmark: Bookmark) {
     this.data.push(bookmark)
