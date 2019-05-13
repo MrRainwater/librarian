fe86602236d857ddd1e4043db5ac98af938e4310
Quinzell <jared.rainwater@monsanto.com>
2019-05-13 07:40:12 -0500

    crudely display bookmarks

--- frontend/src/components/Bookmark.tsx
+++ frontend/src/components/Bookmark.tsx
@@ -8,6 +8,7 @@ import {
   CardActions
 } from 'react-toolbox/lib/card'
 import { IBookmark } from 'interfaces'
+import { IconButton } from 'react-toolbox/lib/button'
 
 interface Props {
   bookmark: IBookmark
@@ -19,6 +20,11 @@ const Bookmark: React.FunctionComponent<Props> = ({ bookmark, className }) => (
     <CardMedia image={bookmark.img} aspectRatio="wide" />
     <CardTitle title={bookmark.title} />
     <CardText>{bookmark.description}</CardText>
+    <CardActions>
+      <a href={bookmark.url} target="_blank">
+        <IconButton icon="link" />
+      </a>
+    </CardActions>
   </Card>
 )
 
