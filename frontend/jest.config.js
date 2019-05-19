d74122442fce27126503cec71865ab4878adf184
Quinzell <jared.rainwater@monsanto.com>
2019-05-19 17:02:43 -0500

    more config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   rootDir: './',
   roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/tests/'],
-  setupFiles: ['<rootDir>/tests/setup.ts']
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules'
+  }
 }
