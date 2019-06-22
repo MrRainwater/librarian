6183491f4da5c5ed149ab7a77f3710b165afe31c
Quinzell <jared.rainwater@monsanto.com>
2019-06-22 15:34:23 -0500

    jest-enzyme

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
+  setupFilesAfterEnv: ['jest-enzyme'],
+  testEnvironment: 'enzyme',
   rootDir: './',
   roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/tests/'],
