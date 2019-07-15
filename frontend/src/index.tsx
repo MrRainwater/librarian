eae948f9bab613a0b275645073a8840208c62aac
Quinzell <jared.rainwater@monsanto.com>
2019-07-14 19:38:13 -0500

    don't need mobx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { Provider } from 'mobx-react'
 import Main from 'components/Main'
 import { BookmarksStoreProvider } from 'stores/BookmarkStore'
 
 ReactDOM.render(
-  <Provider>
-    <BookmarksStoreProvider>
-      <Main />
-    </BookmarksStoreProvider>
-  </Provider>,
+  <BookmarksStoreProvider>
+    <Main />
+  </BookmarksStoreProvider>,
   document.getElementById('app')
 )
