73f6a999ada11f7671e8878f2adea5bc5c19ea60
Quinzell <jared.rainwater@monsanto.com>
2019-10-23 17:19:53 -0500

    not needed dispatch

--- frontend/src/components/Bookmarks/Bookmarks.tsx
+++ frontend/src/components/Bookmarks/Bookmarks.tsx
@@ -2,14 +2,12 @@ import * as React from 'react'
 import * as classes from './styles/Bookmarks.scss'
 import { IBookmark } from 'interfaces'
 import Bookmark from './Bookmark'
-import { useBookmarksStore } from 'stores/BookmarkStore'
 
 interface Props {
   bookmarks: IBookmark[]
 }
 
 const Bookmarks: React.FunctionComponent<Props> = ({ bookmarks }) => {
-  const [state, dispatch] = useBookmarksStore()
   return (
     <div className={classes.bookmarks}>
       {bookmarks.map(bookmark => (
