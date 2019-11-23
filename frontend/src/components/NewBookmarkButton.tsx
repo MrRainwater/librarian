4f1193cbae8157f1a29a51ddf05d2ab77c60d777
Quinzell <jared.rainwater@monsanto.com>
2019-11-23 14:59:31 -0600

    NewBookmarkButton to material

--- frontend/src/components/NewBookmarkButton.tsx
+++ frontend/src/components/NewBookmarkButton.tsx
@@ -1,5 +1,6 @@
+import { Fab } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
 import * as React from 'react'
-import { Button } from 'react-toolbox/lib/button'
 import * as classes from './styles/NewBookmarkButton.scss'
 
 interface IProps {
@@ -7,13 +8,9 @@ interface IProps {
 }
 
 const NewBookmarkButton: React.FunctionComponent<IProps> = ({ onClick }) => (
-  <Button
-    className={classes.button}
-    floating
-    accent
-    icon="add"
-    onClick={onClick}
-  />
+  <Fab className={classes.button} onClick={onClick}>
+    <AddIcon />
+  </Fab>
 )
 
 export default NewBookmarkButton
