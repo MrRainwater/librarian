7da0205fee296c874b0a713064c4a44090c19377
jquinzell <>
2020-05-17 15:06:11 -0500

    clean up tests for rewriting

--- src/components/Global/Main.test.jsx
+++ src/components/Global/Main.test.jsx
@@ -3,18 +3,14 @@ import { act } from 'react-dom/test-utils'
 import { useBookmarksStore } from 'stores/BookmarkStore'
 import Main from 'components/Global/Main'
 import { shallow, mount } from 'enzyme'
-import { getInitial } from 'api'
 import { BookmarkFactory } from '../../factories/BookmarkFactory'
 import Bookmarks from 'components/Bookmarks/Bookmarks'
-import BookmarkFilter from 'components/Global/BookmarkFilter'
 
 jest.mock('stores/BookmarkStore')
-jest.mock('api')
 
 describe('Main', () => {
   beforeEach(() => {
     useBookmarksStore.mockReturnValue([{ bookmarks: [] }, jest.fn()])
-    getInitial.mockResolvedValue({ folders: [], bookmarks: [] })
   })
 
   describe('load', () => {
@@ -31,32 +27,6 @@ describe('Main', () => {
 
       expect(dispatch).toBeCalledWith({ type: 'SET_BOOKMARKS', bookmarks })
       expect(component.find(Bookmarks)).toHaveProp({ bookmarks })
-    })
-  })
-
-  describe('BookmarkFilter', () => {
-    it('passed bookmarks from store', () => {
-      const bookmarks = [BookmarkFactory.build(), BookmarkFactory.build()]
-      useBookmarksStore.mockReturnValue([{ bookmarks }, jest.fn()])
-      const component = shallow(<Main />)
-
-      expect(component.find(BookmarkFilter)).toHaveProp({ bookmarks })
-    })
-
-    it('changes filtered bookmarks', () => {
-      const [bookmark1, bookmark2] = [
-        BookmarkFactory.build(),
-        BookmarkFactory.build()
-      ]
-      useBookmarksStore.mockReturnValue([
-        { bookmarks: [bookmark1, bookmark2] },
-        jest.fn()
-      ])
-      const component = shallow(<Main />)
-
-      component.find(BookmarkFilter).prop('onResults')([bookmark1])
-
-      expect(component.find(Bookmarks)).toHaveProp({ bookmarks: [bookmark1] })
     })
   })
 })
