ce122b31839ab80d532ea224ef159c4f7de65875
jquinzell <>
2020-05-17 14:11:58 -0500

    remove ugly link styles

--- frontend/src/components/Bookmarks/BookmarkListItem.tsx
+++ frontend/src/components/Bookmarks/BookmarkListItem.tsx
@@ -4,8 +4,16 @@ import {
   Box,
   Icon,
   ListItemText,
-  ListItemIcon
+  ListItemIcon,
+  makeStyles
 } from '@material-ui/core'
+
+const useStyles = makeStyles(() => ({
+  link: {
+    color: 'inherit',
+    textDecoration: 'inherit'
+  }
+}))
 
 interface IProps {
   icon: 'bookmark' | 'folder'
@@ -13,19 +21,28 @@ interface IProps {
   url?: string
 }
 
-const BookmarkListItem: React.FC<IProps> = ({ icon, title, url }) => (
-  <ListItem>
-    <Box p={1}>
-      <ListItemIcon>
-        <Icon>{icon}</Icon>
-      </ListItemIcon>
-    </Box>
-    <Box ml={1}>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <ListItemText primary={title} />
-      </a>
-    </Box>
-  </ListItem>
-)
+const BookmarkListItem: React.FC<IProps> = ({ icon, title, url }) => {
+  const styles = useStyles()
+
+  return (
+    <ListItem>
+      <Box p={1}>
+        <ListItemIcon>
+          <Icon>{icon}</Icon>
+        </ListItemIcon>
+      </Box>
+      <Box ml={1}>
+        <a
+          className={styles.link}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ListItemText primary={title} />
+        </a>
+      </Box>
+    </ListItem>
+  )
+}
 
 export default BookmarkListItem
