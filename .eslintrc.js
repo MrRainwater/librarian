d536dd4085c0defe3fbed2b55d68ac84ba690a2d
jquinzell <>
2020-05-17 14:40:49 -0500

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'react'
+    'plugin:react/recommended'
   ],
   parserOptions: {
     project: ['./tsconfig.json']
